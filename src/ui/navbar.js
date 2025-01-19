e713d348ca6a496c869c878fe4ae51a67a20e6d0
csepzol <csepe.zoltan@gmail.com>
2025-01-19 20:37:07 +0100

    Add navbar changes

--- src/ui/navbar.js
+++ src/ui/navbar.js
@@ -20,125 +20,119 @@ class NavigationBar {
 
   #getMenus() {
     return [
-      {
-        name     : 'File',
-        elements : [
-          {
-            name  : 'Open TopoDroid file',
-            click : () => {
-              document.getElementById('topodroidInput').click();
-            }
-          },
-          {
-            name  : 'Open Polygon file',
-            click : function () {
-              document.getElementById('polygonInput').click();
-            }
-          },
-          {
-            name  : 'Open JSON file',
-            click : function () {
-              document.getElementById('jsonInput').click();
-            }
-          },
-          { name: 'Export JSON', click: () => Exporter.exportCaves(this.db.caves) },
-          { name: 'Export PNG', click: () => Exporter.exportPNG(this.scene) }
-        ]
-      },
-      {
-        name     : 'Surface',
-        elements : [
-          {
-            name  : 'Open PLY file',
-            click : function () {
-              document.getElementById('plyInput').click();
-            }
-          }
-        ]
-      }
+        {
+            name: i18next.t('File'),
+            elements: [
+                {
+                    name: i18next.t('Open TopoDroid file'),
+                    click: () => {
+                        document.getElementById('topodroidInput').click();
+                    }
+                },
+                {
+                    name: i18next.t('Open Polygon file'),
+                    click: function () {
+                        document.getElementById('polygonInput').click();
+                    }
+                },
+                {
+                    name: i18next.t('Open JSON file'),
+                    click: function () {
+                        document.getElementById('jsonInput').click();
+                    }
+                },
+                { name: i18next.t('Export JSON'), click: () => Exporter.exportCaves(this.db.caves) },
+                { name: i18next.t('Export PNG'), click: () => Exporter.exportPNG(this.scene) }
+            ]
+        },
+        {
+            name: i18next.t('Surface'),
+            elements: [
+                {
+                    name: i18next.t('Open PLY file'),
+                    click: function () {
+                        document.getElementById('plyInput').click();
+                    }
+                }
+            ]
+        }
     ];
-  }
+}
 
-  #getIcons() {
-    return [
+#getIcons() {
+  return [
       {
-        tooltip : 'Print',
-        icon    : './icons/print.svg',
-        click   : () => window.print()
+          tooltip: i18next.t('Print'),
+          icon: './icons/print.svg',
+          click: () => window.print()
       },
       {
-        tooltip : 'Zoom to fit',
-        icon    : './icons/zoom_fit.svg',
-        click   : () => this.scene.fitScene(this.scene.computeBoundingBox())
+          tooltip: i18next.t('Zoom to fit'),
+          icon: './icons/zoom_fit.svg',
+          click: () => this.scene.fitScene(this.scene.computeBoundingBox())
       },
       {
-        tooltip : 'Zoom in',
-        icon    : './icons/zoom_in.svg',
-        click   : () => this.scene.zoomWithStep(this.options.scene.zoomStep)
+          tooltip: i18next.t('Zoom in'),
+          icon: './icons/zoom_in.svg',
+          click: () => this.scene.zoomWithStep(this.options.scene.zoomStep)
       },
       {
-        tooltip : 'Zoom out',
-        icon    : './icons/zoom_out.svg',
-        click   : () => this.scene.zoomWithStep(-1 * this.options.scene.zoomStep)
+          tooltip: i18next.t('Zoom out'),
+          icon: './icons/zoom_out.svg',
+          click: () => this.scene.zoomWithStep(-1 * this.options.scene.zoomStep)
       },
       {
-        tooltip : 'Plan',
-        icon    : './icons/plan.svg',
-        click   : () => this.scene.lookAtPlan()
+          tooltip: i18next.t('Plan'),
+          icon: './icons/plan.svg',
+          click: () => this.scene.lookAtPlan()
       },
       {
-        tooltip : 'Profile',
-        icon    : './icons/profile.svg',
-        click   : () => this.scene.lookAtProfile()
+          tooltip: i18next.t('Profile'),
+          icon: './icons/profile.svg',
+          click: () => this.scene.lookAtProfile()
       },
       {
-        tooltip : '3D',
-        icon    : './icons/3d.svg',
-        click   : () => this.scene.lookAt3D()
+          tooltip: i18next.t('3D'),
+          icon: './icons/3d.svg',
+          click: () => this.scene.lookAt3D()
       },
       {
-        tooltip : 'Bounding box',
-        icon    : './icons/bounding_box.svg',
-        click   : () => this.scene.toogleBoundingBox()
+          tooltip: i18next.t('Bounding box'),
+          icon: './icons/bounding_box.svg',
+          click: () => this.scene.toogleBoundingBox()
       },
       {
-        tooltip : 'Show beddings',
-        icon    : './icons/bedding.svg',
-        click   : () => this.scene.tooglePlaneFor('bedding')
+          tooltip: i18next.t('Show beddings'),
+          icon: './icons/bedding.svg',
+          click: () => this.scene.tooglePlaneFor('bedding')
       },
       {
-        tooltip : 'Show faults',
-        icon    : './icons/fault.svg',
-        click   : () => this.scene.tooglePlaneFor('fault')
+          tooltip: i18next.t('Show faults'),
+          icon: './icons/fault.svg',
+          click: () => this.scene.tooglePlaneFor('fault')
       },
       {
-        tooltip : 'Line color mode',
-        icon    : './icons/cl_color.svg',
-        click   : () => this.scene.rollCenterLineColor()
+          tooltip: i18next.t('Line color mode'),
+          icon: './icons/cl_color.svg',
+          click: () => this.scene.rollCenterLineColor()
       },
       {
-        tooltip : 'Grid position/visibility',
-        icon    : './icons/grid.svg',
-        click   : () => this.scene.grid.roll()
+          tooltip: i18next.t('Grid position/visibility'),
+          icon: './icons/grid.svg',
+          click: () => this.scene.grid.roll()
       },
       {
-        tooltip : 'Surface visibility',
-        icon    : './icons/surface.svg',
-        click   : () => this.scene.rollSurface()
+          tooltip: i18next.t('Surface visibility'),
+          icon: './icons/surface.svg',
+          click: () => this.scene.rollSurface()
       },
       {
-        tooltip : 'Locate point',
-        icon    : './icons/locate.svg',
-        click   : (event) => this.interactive.showLocateStationPanel(event.clientX)
-      },
-      {
-        tooltip : 'Distance between points',
-        icon    : './icons/distance.svg',
-        click   : (event) => this.interactive.showShortestPathPanel(event.clientX)
+          tooltip: i18next.t('Locate point'),
+          icon: './icons/locate.svg',
+          click: (event) => this.interactive.showLocatePanel(event.clientX)
       }
-
-    ];
-  }
+  ];
+}
 
   #addNavbarClickListener() {
     //Close the dropdown if the user clicks outside of it
