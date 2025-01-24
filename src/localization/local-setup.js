e733356adec0891226d951ec9ffbfbf26d22aea0
csepzol <csepe.zoltan@gmail.com>
2025-01-24 12:06:34 +0100

    Run prettier

--- src/localization/local-setup.js
+++ src/localization/local-setup.js
@@ -1,60 +1,60 @@
 i18next.init({
-  lng: 'hu', // set the default language
-  resources: {
-    en: {
-      translation: {
-        "test": "Test",
-        "File": "File",
-        "Open TopoDroid file": "Open TopoDroid file",
-        "Open Polygon file": "Open Polygon file",
-        "Open JSON file": "Open JSON file",
-        "Export JSON": "Export JSON",
-        "Export PNG": "Export PNG",
-        "Surface": "Surface",
-        "Open PLY file": "Open PLY file",
-        "Print": "Print",
-        "Zoom to fit": "Zoom to fit",
-        "Zoom in": "Zoom in",
-        "Zoom out": "Zoom out",
-        "Plan": "Plan",
-        "Profile": "Profile",
-        "3D": "3D",
-        "Bounding box": "Bounding box",
-        "Show beddings": "Show beddings",
-        "Show faults": "Show faults",
-        "Line color mode": "Line color mode",
-        "Grid position/visibility": "Grid position/visibility",
-        "Surface visibility": "Surface visibility",
-        "Locate point": "Locate point"
+  lng       : 'hu', // set the default language
+  resources : {
+    en : {
+      translation : {
+        test                       : 'Test',
+        File                       : 'File',
+        'Open TopoDroid file'      : 'Open TopoDroid file',
+        'Open Polygon file'        : 'Open Polygon file',
+        'Open JSON file'           : 'Open JSON file',
+        'Export JSON'              : 'Export JSON',
+        'Export PNG'               : 'Export PNG',
+        Surface                    : 'Surface',
+        'Open PLY file'            : 'Open PLY file',
+        Print                      : 'Print',
+        'Zoom to fit'              : 'Zoom to fit',
+        'Zoom in'                  : 'Zoom in',
+        'Zoom out'                 : 'Zoom out',
+        Plan                       : 'Plan',
+        Profile                    : 'Profile',
+        '3D'                       : '3D',
+        'Bounding box'             : 'Bounding box',
+        'Show beddings'            : 'Show beddings',
+        'Show faults'              : 'Show faults',
+        'Line color mode'          : 'Line color mode',
+        'Grid position/visibility' : 'Grid position/visibility',
+        'Surface visibility'       : 'Surface visibility',
+        'Locate point'             : 'Locate point'
       }
-     },
-      hu: {
-          translation: {
-              "test": "Teszt",
-              "File": "Fájl",
-              "Open TopoDroid file": "TopoDroid fájl megnyitása",
-              "Open Polygon file": "Polygon fájl megnyitása",
-              "Open JSON file": "JSON fájl megnyitása",
-              "Export JSON": "Export JSON",
-              "Export PNG": "Export PNG",
-              "Surface": "Felszín",
-              "Open PLY file": "PLY fájl megnyitása",
-              "Print": "Nyomtatás",
-              "Zoom to fit": "Zoom to fit",
-              "Zoom in": "Zoom in",
-              "Zoom out": "Zoom out",
-              "Plan": "Plan",
-              "Profile": "Profile",
-              "3D": "3D",
-              "Bounding box": "Befoglaló doboz",
-              "Show beddings": "Show beddings",
-              "Show faults": "Show faults",
-              "Line color mode": "Line color mode",
-              "Grid position/visibility": "Grid position/visibility",
-              "Surface visibility": "Felszín láthatósága",
-              "Locate point": "Pont lokalizáció"
-          }
-      },
+    },
+    hu : {
+      translation : {
+        test                       : 'Teszt',
+        File                       : 'Fájl',
+        'Open TopoDroid file'      : 'TopoDroid fájl megnyitása',
+        'Open Polygon file'        : 'Polygon fájl megnyitása',
+        'Open JSON file'           : 'JSON fájl megnyitása',
+        'Export JSON'              : 'Export JSON',
+        'Export PNG'               : 'Export PNG',
+        Surface                    : 'Felszín',
+        'Open PLY file'            : 'PLY fájl megnyitása',
+        Print                      : 'Nyomtatás',
+        'Zoom to fit'              : 'Zoom to fit',
+        'Zoom in'                  : 'Zoom in',
+        'Zoom out'                 : 'Zoom out',
+        Plan                       : 'Plan',
+        Profile                    : 'Profile',
+        '3D'                       : '3D',
+        'Bounding box'             : 'Befoglaló doboz',
+        'Show beddings'            : 'Show beddings',
+        'Show faults'              : 'Show faults',
+        'Line color mode'          : 'Line color mode',
+        'Grid position/visibility' : 'Grid position/visibility',
+        'Surface visibility'       : 'Felszín láthatósága',
+        'Locate point'             : 'Pont lokalizáció'
+      }
+    }
     // Add other languages here
   }
 });
--- src/ui/navbar.js
+++ src/ui/navbar.js
@@ -20,119 +20,119 @@ class NavigationBar {
 
   #getMenus() {
     return [
-        {
-            name: i18next.t('File'),
-            elements: [
-                {
-                    name: i18next.t('Open TopoDroid file'),
-                    click: () => {
-                        document.getElementById('topodroidInput').click();
-                    }
-                },
-                {
-                    name: i18next.t('Open Polygon file'),
-                    click: function () {
-                        document.getElementById('polygonInput').click();
-                    }
-                },
-                {
-                    name: i18next.t('Open JSON file'),
-                    click: function () {
-                        document.getElementById('jsonInput').click();
-                    }
-                },
-                { name: i18next.t('Export JSON'), click: () => Exporter.exportCaves(this.db.caves) },
-                { name: i18next.t('Export PNG'), click: () => Exporter.exportPNG(this.scene) }
-            ]
-        },
-        {
-            name: i18next.t('Surface'),
-            elements: [
-                {
-                    name: i18next.t('Open PLY file'),
-                    click: function () {
-                        document.getElementById('plyInput').click();
-                    }
-                }
-            ]
-        }
-    ];
-}
-
-#getIcons() {
-  return [
       {
-          tooltip: i18next.t('Print'),
-          icon: './icons/print.svg',
-          click: () => window.print()
+        name     : i18next.t('File'),
+        elements : [
+          {
+            name  : i18next.t('Open TopoDroid file'),
+            click : () => {
+              document.getElementById('topodroidInput').click();
+            }
+          },
+          {
+            name  : i18next.t('Open Polygon file'),
+            click : function () {
+              document.getElementById('polygonInput').click();
+            }
+          },
+          {
+            name  : i18next.t('Open JSON file'),
+            click : function () {
+              document.getElementById('jsonInput').click();
+            }
+          },
+          { name: i18next.t('Export JSON'), click: () => Exporter.exportCaves(this.db.caves) },
+          { name: i18next.t('Export PNG'), click: () => Exporter.exportPNG(this.scene) }
+        ]
       },
       {
-          tooltip: i18next.t('Zoom to fit'),
-          icon: './icons/zoom_fit.svg',
-          click: () => this.scene.fitScene(this.scene.computeBoundingBox())
-      },
-      {
-          tooltip: i18next.t('Zoom in'),
-          icon: './icons/zoom_in.svg',
-          click: () => this.scene.zoomWithStep(this.options.scene.zoomStep)
-      },
-      {
-          tooltip: i18next.t('Zoom out'),
-          icon: './icons/zoom_out.svg',
-          click: () => this.scene.zoomWithStep(-1 * this.options.scene.zoomStep)
-      },
-      {
-          tooltip: i18next.t('Plan'),
-          icon: './icons/plan.svg',
-          click: () => this.scene.lookAtPlan()
-      },
-      {
-          tooltip: i18next.t('Profile'),
-          icon: './icons/profile.svg',
-          click: () => this.scene.lookAtProfile()
-      },
-      {
-          tooltip: i18next.t('3D'),
-          icon: './icons/3d.svg',
-          click: () => this.scene.lookAt3D()
-      },
-      {
-          tooltip: i18next.t('Bounding box'),
-          icon: './icons/bounding_box.svg',
-          click: () => this.scene.toogleBoundingBox()
-      },
-      {
-          tooltip: i18next.t('Show beddings'),
-          icon: './icons/bedding.svg',
-          click: () => this.scene.tooglePlaneFor('bedding')
-      },
-      {
-          tooltip: i18next.t('Show faults'),
-          icon: './icons/fault.svg',
-          click: () => this.scene.tooglePlaneFor('fault')
-      },
-      {
-          tooltip: i18next.t('Line color mode'),
-          icon: './icons/cl_color.svg',
-          click: () => this.scene.rollCenterLineColor()
-      },
-      {
-          tooltip: i18next.t('Grid position/visibility'),
-          icon: './icons/grid.svg',
-          click: () => this.scene.grid.roll()
-      },
-      {
-          tooltip: i18next.t('Surface visibility'),
-          icon: './icons/surface.svg',
-          click: () => this.scene.rollSurface()
-      },
-      {
-          tooltip: i18next.t('Locate point'),
-          icon: './icons/locate.svg',
-          click: (event) => this.interactive.showLocatePanel(event.clientX)
+        name     : i18next.t('Surface'),
+        elements : [
+          {
+            name  : i18next.t('Open PLY file'),
+            click : function () {
+              document.getElementById('plyInput').click();
+            }
+          }
+        ]
       }
-  ];
-}
+    ];
+  }
+
+  #getIcons() {
+    return [
+      {
+        tooltip : i18next.t('Print'),
+        icon    : './icons/print.svg',
+        click   : () => window.print()
+      },
+      {
+        tooltip : i18next.t('Zoom to fit'),
+        icon    : './icons/zoom_fit.svg',
+        click   : () => this.scene.fitScene(this.scene.computeBoundingBox())
+      },
+      {
+        tooltip : i18next.t('Zoom in'),
+        icon    : './icons/zoom_in.svg',
+        click   : () => this.scene.zoomWithStep(this.options.scene.zoomStep)
+      },
+      {
+        tooltip : i18next.t('Zoom out'),
+        icon    : './icons/zoom_out.svg',
+        click   : () => this.scene.zoomWithStep(-1 * this.options.scene.zoomStep)
+      },
+      {
+        tooltip : i18next.t('Plan'),
+        icon    : './icons/plan.svg',
+        click   : () => this.scene.lookAtPlan()
+      },
+      {
+        tooltip : i18next.t('Profile'),
+        icon    : './icons/profile.svg',
+        click   : () => this.scene.lookAtProfile()
+      },
+      {
+        tooltip : i18next.t('3D'),
+        icon    : './icons/3d.svg',
+        click   : () => this.scene.lookAt3D()
+      },
+      {
+        tooltip : i18next.t('Bounding box'),
+        icon    : './icons/bounding_box.svg',
+        click   : () => this.scene.toogleBoundingBox()
+      },
+      {
+        tooltip : i18next.t('Show beddings'),
+        icon    : './icons/bedding.svg',
+        click   : () => this.scene.tooglePlaneFor('bedding')
+      },
+      {
+        tooltip : i18next.t('Show faults'),
+        icon    : './icons/fault.svg',
+        click   : () => this.scene.tooglePlaneFor('fault')
+      },
+      {
+        tooltip : i18next.t('Line color mode'),
+        icon    : './icons/cl_color.svg',
+        click   : () => this.scene.rollCenterLineColor()
+      },
+      {
+        tooltip : i18next.t('Grid position/visibility'),
+        icon    : './icons/grid.svg',
+        click   : () => this.scene.grid.roll()
+      },
+      {
+        tooltip : i18next.t('Surface visibility'),
+        icon    : './icons/surface.svg',
+        click   : () => this.scene.rollSurface()
+      },
+      {
+        tooltip : i18next.t('Locate point'),
+        icon    : './icons/locate.svg',
+        click   : (event) => this.interactive.showLocatePanel(event.clientX)
+      }
+    ];
+  }
 
   #addNavbarClickListener() {
     //Close the dropdown if the user clicks outside of it
